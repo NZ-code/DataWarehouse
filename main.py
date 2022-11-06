0e1fe9c6565d14032efea663794b3373b591c4e9
Nick <zenevichnikita2001@gmail.com>
2022-11-06 16:39:59 +0100

    K

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from faker import Faker
 import random
-import datetime
+from datetime import timedelta, date
 from Contract import Contract
 from Consultation import Consultation
 from Client import Client
@@ -15,17 +15,43 @@ def add_contract_employee(employees: list, contracts: list):
             contract.add_employee(employee)
             employee.add_contract(contract)
 
-
+def genEmployee(start_range:date, end_range:date):
+    # case 1 : company start = start_range  T1 = end_range
+    fake = Faker()
+    hire_date =fake.date_between(start_range, end_range)
+    
+    min_work_days = 60
+    min_month = timedelta(days=min_work_days)
+    max_work_days = (end_range - hire_date).days
+    
+    chance_of_firing = 10
+    fire_date = None
+    if max_work_days > min_work_days and random.randint(0,100) < chance_of_firing:
+        work_duration = random.randint(min_work_days,max_work_days)
+        fire_date  = hire_date + timedelta(days = work_duration)
+    return Employee(fake.name(), hire_date, fire_date)
+def genConsultation(employees:list,end_range:date):
+    fake = Faker()
+    consultant = employees.choise()
+    consultation_date = fake.date_between(consultant.hire_date, consultant.fire_date or end_range)
+    
+  
 def main():
+    company_start = date(year=2006, month=3, day=20)
+    t1 = date(year=2010, month=8, day=27)
     number = 10
     fake = Faker()
+    
+    employees = []
+    for i in range(number):
+        employees.append(genEmployee(company_start, t1))
+
+
     clients = []
     for i in range(number):
         clients.append(Client(fake.name()))
 
-    employees = []
-    for i in range(number):
-        employees.append(Employee(fake.name(), fake.date(), fake.date()))
+    
 
     contracts = []
     for i in range(number):
