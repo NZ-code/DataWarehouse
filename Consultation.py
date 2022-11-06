e5df9158776ce86160c45abe664eb3977b843e81
Wladyslaw Bogusz <bogush22092209@gmail.com>
2022-11-06 17:38:28 +0100

    very stupid change

--- Consultation.py
+++ Consultation.py
@@ -1,21 +1,45 @@
 from Employee import Employee
 from Contract import Contract
+from datetime import timedelta, date
+import random
+from Contract import ServiceType
+from Client import Client
+
+
+def add_contract_employee(employee: Employee, contract: Contract):
+    contract.add_employee(employee)
+    employee.add_contract(contract)
+
+
+def _gen_contract(client: Client, consultation_date: date):
+    max_contract_end_date = date(2030, 1, 1)
+    date_start = consultation_date
+    min_duration_days = 180
+    contract_duration = random.randint(min_duration_days, (max_contract_end_date - date_start).days)
+    date_end = date_start + timedelta(days=contract_duration)
+
+    return Contract(random.randint(20, 100), client, date_start, date_end,
+                    random.choice(list(ServiceType)), bool(random.getrandbits(1)))
+
 
 class Consultation:
     idGen = 0
 
-    def __init__(self, duration, hourly_rate, employee: Employee, contract: Contract, date):
+    def __init__(self, duration, hourly_rate, employee: Employee, consultation_date: date, client: Client,
+                 employees: list):
         Consultation.idGen += 1
         self.id = self.idGen
         self.duration = duration
         self.hourly_rate = hourly_rate
         self.employee = employee
-        self.contract = contract
-        self.date = date
+        self.consultation_date = consultation_date
+        self.contract = _gen_contract(client, consultation_date)
+        
 
     def __str__(self):
         return "Consultation( id:" + str(self.id) + ", duration:" + str(self.duration) + ", hourly_rate:" + str(
-            self.hourly_rate) + ", " + str(self.employee) + ", " + str(self.contract) + ", date:" + str(self.date) + ")"
+            self.hourly_rate) + ", " + str(self.employee) + ", " + str(self.contract) + ", consultation_date:" + str(
+            self.consultation_date) + ")"
 
     def __repr__(self):
         return str(self)
--- main.py
+++ main.py
@@ -8,75 +8,58 @@ from Employee import Employee
 from Contract import ServiceType
 
 
-def add_contract_employee(employees: list, contracts: list):
-    for contract in contracts:
-        for i in range(random.randint(1, 5)):
-            employee = random.choice(employees)
-            contract.add_employee(employee)
-            employee.add_contract(contract)
-
-def genEmployee(start_range:date, end_range:date):
+def gen_employee(start_range: date, end_range: date):
     # case 1 : company start = start_range  T1 = end_range
     fake = Faker()
-    hire_date =fake.date_between(start_range, end_range)
-    
+    hire_date = fake.date_between(start_range, end_range)
+
     min_work_days = 60
     min_month = timedelta(days=min_work_days)
     max_work_days = (end_range - hire_date).days
-    
+
     chance_of_firing = 10
     fire_date = None
-    if max_work_days > min_work_days and random.randint(0,100) < chance_of_firing:
-        work_duration = random.randint(min_work_days,max_work_days)
-        fire_date  = hire_date + timedelta(days = work_duration)
+    if max_work_days > min_work_days and random.randint(0, 100) < chance_of_firing:
+        work_duration = random.randint(min_work_days, max_work_days)
+        fire_date = hire_date + timedelta(days=work_duration)
     return Employee(fake.name(), hire_date, fire_date)
-def genConsultation(employees:list,end_range:date):
+
+
+def gen_consultation(employees: list, end_range: date, clients: list):
     fake = Faker()
-    consultant = employees.choise()
+    consultant = random.choice(employees)
     consultation_date = fake.date_between(consultant.hire_date, consultant.fire_date or end_range)
-    
-  
+    return Consultation(random.randint(2, 8), random.randint(20, 100), consultant, consultation_date,
+                        random.choice(clients))
+
+
+
+
+
 def main():
     company_start = date(year=2006, month=3, day=20)
     t1 = date(year=2010, month=8, day=27)
     number = 10
     fake = Faker()
-    
+
     employees = []
     for i in range(number):
-        employees.append(genEmployee(company_start, t1))
-
+        employees.append(gen_employee(company_start, t1))
 
     clients = []
     for i in range(number):
         clients.append(Client(fake.name()))
 
-    
-
-    contracts = []
-    for i in range(number):
-        contracts.append(
-            Contract(random.randint(20, 100), random.choice(clients), fake.date(), fake.date(),
-                     random.choice(list(ServiceType)), bool(random.getrandbits(1))))
-
-    add_contract_employee(employees,contracts)
-
     consultations = []
     for i in range(number):
-        consultations.append(Consultation(random.randint(2, 8), random.randint(20, 100), random.choice(employees),
-                                          random.choice(contracts), fake.date()))
+        consultations.append(gen_consultation(employees, t1, clients))
 
     [print(object) for object in clients]
     print()
     [print(object) for object in consultations]
     print()
-    [print(object) for object in contracts]
-    print()
     [print(object) for object in employees]
 
-    for contract in contracts:
-        for employee in contract.get_employees():
-            print(str(contract.id) +","+str(employee.id))
 
 if __name__ == '__main__':
     main()
